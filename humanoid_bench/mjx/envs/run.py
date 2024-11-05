f379aac45ede4778f63bebd71e36a1c218ab605c
Sagar Verma <sagar15056@iiitd.ac.in>
2024-11-05 02:35:52 -0500

    run fix

--- humanoid_bench/mjx/envs/run.py
+++ humanoid_bench/mjx/envs/run.py
@@ -94,9 +94,7 @@ class HumanoidRunPosControl(MjxEnv):
                        "small_control": 0., 
                        "move": 0.,
                        "standing": 0.,
-                       "upright": 0.,
-                       "actuator_effort": 0.,
-                       "energy_efficiency_penalty": 0.
+                       "upright": 0.
                        })
         
         return state
@@ -123,14 +121,6 @@ class HumanoidRunPosControl(MjxEnv):
         # Basic reward with movement
         reward = small_control * stand_reward * move
 
-        # Energy efficiency penalty calculation
-        actuator_effort = jp.sum(data.data.qfrc_actuator**2)
-        normalized_effort = jp.sqrt(actuator_effort) / data.data.qfrc_actuator.shape[0]
-        energy_efficiency_penalty = -1e-2 * normalized_effort * stand_reward  # Adjust weight as needed
-
-        # Final reward integration
-        reward += energy_efficiency_penalty
-
         # Termination condition
         terminated = jp.where(data.data.qpos[2] < 0.2, 1.0, 0.0)
         reward = jp.where(jp.isnan(reward), 0, reward)
@@ -140,9 +130,7 @@ class HumanoidRunPosControl(MjxEnv):
             "small_control": small_control, 
             "move": move,
             "standing": standing,
-            "upright": upright,
-            "actuator_effort": actuator_effort,
-            "energy_efficiency_penalty": energy_efficiency_penalty
+            "upright": upright
         }
 
         return reward, terminated, sub_rewards
